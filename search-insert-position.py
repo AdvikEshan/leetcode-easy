5900fbcadad1af0ce2b615b1694d69322d1f56b7
Nitish Tiwari <kumarnitish1996@gmail.com>
2020-05-05 15:27:10 +0530

    day-3

--- search-insert-position.py
+++ search-insert-position.py
@@ -1,10 +1,18 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        if target in nums:
-            return nums.index(target)
-        elif target > nums[len(nums)-1]:
-            return len(nums)
-        else:
-            for i in range(len(nums)):
-                if nums[i]>target: 
-                    return nums.index(nums[i])
+        # This one is better -> time complexity
+        
+        # if target in nums:
+        #     return nums.index(target)
+        # elif target > nums[len(nums)-1]:
+        #     return len(nums)
+        # else:
+        #     for i in range(len(nums)):
+        #         if nums[i]>target: 
+        #             return nums.index(nums[i])
+        
+        for index,item in enumerate(nums):
+            if item >= target:
+                return index
+            continue
+        return len(nums)
