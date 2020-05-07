ff95e53ad46cf8e7c2dc1bedb340dd43e5bfa3aa
Nitish Tiwari <kumarnitish1996@gmail.com>
2020-05-07 19:11:55 +0530

    day-4

--- merge-sorted-array.py
+++ merge-sorted-array.py
@@ -3,27 +3,27 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        del nums1[m:len(nums1)]
-        for i in range(n):
-            nums1.append(nums2[i])
-        nums1.sort()     
-#         i = j = 0
-#         del nums1[m:len(nums1)]
-#         while i < m and j < n:
-#             if nums1[i] < nums2[j]:
-#                 nums1[i]=nums1[i]
-#                 i += 1
-#             else:
-#                 i += 1
-#                 nums1.insert(i,nums2[j])
-#                 j += 1
-        
-#         while j < n:
-#             nums1.append(nums2[j])
-#             j += 1
-            
-#         return nums1 
-
         # nums1[m:]=nums2
         # return nums1.sort()
+    
+#         del nums1[m:len(nums1)]
+#         for i in range(n):
+#             nums1.append(nums2[i])
+#         nums1.sort() 
             
+        
+        i = m-1
+        j = n-1
+        l = m+n-1
+        
+        while j >= 0:
+            if i >= 0 and nums1[i] > nums2[j]:
+                nums1[l]=nums1[i]
+                i -= 1
+            else:
+                nums1[l] = nums2[j]
+                j -= 1
+            l -= 1
+        
+            
+        return nums1             
